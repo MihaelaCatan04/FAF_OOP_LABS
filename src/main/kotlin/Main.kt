ecdbac1f82eee4adc32fd135e1a1eda97e11c0de
Mihaela Catan <mihaelacatan7@gmail.com>
2024-10-09 04:58:42 +0300

    create a simple class for Day 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,43 @@
-package org.example
+data class Creature(
+    private var id: Int,
+    private var isHumanoid: Boolean,
+    private var planet: String,
+    private var age: Int,
+    private var traits: List<String>
+) {
+    // Getter methods to access private properties
+    fun getId() = id
+    fun getIsHumanoid() = isHumanoid
+    fun getPlanet() = planet
+    fun getAge() = age
+    fun getTraits() = traits
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    // Methods
+    fun greet(){
+        println("Hello! I am creature No. ${id}")
     }
+
+    fun printInfo() {
+        println("Creature: ${id}")
+        println("Is Humanoid? ${isHumanoid}")
+        println("It is from ${planet}")
+        println("It is ${age} years old")
+        println("It is ${traits}")
+    }
+}
+
+
+fun main() {
+
+    // Create instances of the Creature class
+    val creature1 = Creature(1, false, "Mars", 200, listOf("tall", "green"))
+    val creature2 = Creature(2, true, "Earth", 16, listOf("intelligent", "blue-eyed"))
+
+    for (creature in listOf(creature1, creature2)) {
+        creature.printInfo()
+    }
+
+    val creature3 = creature2.copy(id = 3, isHumanoid = false)
+    creature3.printInfo()
+    creature3.greet()
 }
