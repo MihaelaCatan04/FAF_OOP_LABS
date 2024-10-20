a4d193899347b73310320411101698295e3a5da6
Mihaela Catan <mihaelacatan7@gmail.com>
2024-10-20 21:28:33 +0300

    optimized classification method

--- src/main/kotlin/UniverseClassifier.kt
+++ src/main/kotlin/UniverseClassifier.kt
@@ -13,67 +13,17 @@ class UniverseClassifier {
     private fun requiredTraits(creatureTraits: List<Traits>?, trait1: Traits, trait2: Traits): Boolean {
         if (creatureTraits.isNullOrEmpty()) {
             return true
-        }
-        return if (creatureTraits.size > 1) {
-            trait1 in creatureTraits && trait2 in creatureTraits
+        } else if (creatureTraits.size > 1) {
+            return trait1 in creatureTraits && trait2 in creatureTraits
         } else {
-            trait1 in creatureTraits || trait2 in creatureTraits
+            return trait1 in creatureTraits || trait2 in creatureTraits
         }
     }
-
-    // Method to classify a Wookie
-    private fun isWookie(creature: Creature): Boolean {
-        return (creature.isHumanoid == false || creature.isHumanoid == null) &&
-                (creature.planet == Planet.Kashyyyk || creature.planet == null) &&
-                (creature.age in 0..400 || creature.age == null) &&
-                (requiredTraits(creature.traits, Traits.HAIRY, Traits.TALL))
-    }
-
-    // Method to classify an Ewok
-    private fun isEwok(creature: Creature): Boolean {
-        return (creature.isHumanoid == false || creature.isHumanoid == null) &&
-                (creature.planet == Planet.Endor || creature.planet == null) &&
-                (creature.age in 0..60 || creature.age == null) &&
-                (requiredTraits(creature.traits, Traits.SHORT, Traits.HAIRY))
-    }
-
-    // Method to classify an Asgardian
-    private fun isAsgardian(creature: Creature): Boolean {
-        return (creature.isHumanoid == true || creature.isHumanoid == null) &&
-                (creature.planet == Planet.Asgard || creature.planet == null) &&
-                (creature.age in 0..5000 || creature.age == null) &&
-                (requiredTraits(creature.traits, Traits.BLONDE, Traits.TALL))
-    }
-
-    // Method to classify a Betelgeusian
-    private fun isBetelgeusian(creature: Creature): Boolean {
-        return (creature.isHumanoid == true || creature.isHumanoid == null) &&
-                (creature.planet == Planet.Betelgeuse || creature.planet == null) &&
-                (creature.age in 0..100 || creature.age == null) &&
-                (requiredTraits(creature.traits, Traits.EXTRA_ARMS, Traits.EXTRA_HEAD))
-    }
-
-    // Method to classify a Vogon
-    private fun isVogon(creature: Creature): Boolean {
-        return (creature.isHumanoid == false || creature.isHumanoid == null) &&
-                (creature.planet == Planet.Vogsphere || creature.planet == null) &&
-                (creature.age in 0..200 || creature.age == null) &&
-                (requiredTraits(creature.traits, Traits.GREEN, Traits.BULKY))
-    }
-
-    // Method to classify an Elf
-    private fun isElf(creature: Creature): Boolean {
-        return (creature.isHumanoid == true || creature.isHumanoid == null) &&
-                (creature.planet == Planet.Earth || creature.planet == null) &&
-                (requiredTraits(creature.traits, Traits.BLONDE, Traits.POINTY_EARS))
-    }
-
-    // Method to classify a Dwarf
-    private fun isDwarf(creature: Creature): Boolean {
-        return (creature.isHumanoid == true || creature.isHumanoid == null) &&
-                (creature.planet == Planet.Earth || creature.planet == null) &&
-                (creature.age in 0..200 || creature.age == null) &&
-                (requiredTraits(creature.traits, Traits.SHORT, Traits.BULKY))
+    private fun checkRequirements(creature: Creature, expectedIsHumanoid: Boolean, expectedPlanet: Planet, expectedAge: Int, trait1: Traits, trait2: Traits):Boolean{
+        return (creature.isHumanoid == expectedIsHumanoid || creature.isHumanoid == null) &&
+                (creature.planet == expectedPlanet || creature.planet == null) &&
+                (creature.age == null || creature.age <= expectedAge) &&
+                (requiredTraits(creature.traits, trait1, trait2))
     }
     // Method to identify a null object
     private fun isAllNull(creature: Creature): Boolean {
@@ -87,10 +37,10 @@ class UniverseClassifier {
         for (creature in creatures) {
             when {
                 isAllNull(creature) -> universes[4].individuals.add(creature)
-                isWookie(creature) || isEwok(creature) -> universes[0].individuals.add(creature)
-                isAsgardian(creature) -> universes[1].individuals.add(creature)
-                isBetelgeusian(creature) || isVogon(creature) -> universes[2].individuals.add(creature)
-                isElf(creature) || isDwarf(creature) -> universes[3].individuals.add(creature)
+                checkRequirements(creature, false, Planet.Kashyyyk, 400, Traits.HAIRY, Traits.TALL) || checkRequirements(creature, false, Planet.Endor, 60, Traits.SHORT, Traits.HAIRY) -> universes[0].individuals.add(creature)
+                checkRequirements(creature, true, Planet.Asgard, 5000, Traits.BLONDE, Traits.TALL) -> universes[1].individuals.add(creature)
+                checkRequirements(creature, true, Planet.Betelgeuse, 100, Traits.EXTRA_ARMS, Traits.EXTRA_HEAD) || checkRequirements(creature, false, Planet.Vogsphere, 200, Traits.GREEN, Traits.BULKY) -> universes[2].individuals.add(creature)
+                checkRequirements(creature, true, Planet.Earth, Int.MAX_VALUE, Traits.BLONDE, Traits.POINTY_EARS) || checkRequirements(creature, true, Planet.Earth, 200, Traits.SHORT, Traits.BULKY) -> universes[3].individuals.add(creature)
                 else -> universes[4].individuals.add(creature)
             }
         }
