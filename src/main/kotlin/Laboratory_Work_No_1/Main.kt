2c3358904a5da439f7862bd87199ea0ff92a9221
Mihaela Catan <66206241+MihaelaCatan04@users.noreply.github.com>
2024-10-23 16:59:55 +0300

    Update Main.kt

--- src/main/kotlin/Laboratory_Work_No_1/Main.kt
+++ src/main/kotlin/Laboratory_Work_No_1/Main.kt
@@ -1,10 +1,5 @@
 package Laboratory_Work_No_1
 
-import Laboratory_Work_No_0.FileReader
-import Laboratory_Work_No_0.OutputWriter
-import Laboratory_Work_No_0.UniverseClassifier
-import Laboratory_Work_No_0.View
-
 fun main() {
     println("Hello, world!")
 }
