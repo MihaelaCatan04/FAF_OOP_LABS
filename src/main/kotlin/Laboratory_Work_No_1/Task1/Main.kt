3520d5a5f0678369087f51ac3f86d0eef5863a3c
Mihaela Catan <mihaelacatan7@gmail.com>
2024-10-23 22:04:38 +0300

    refactor code inside the main class

--- src/main/kotlin/Laboratory_Work_No_1/Task1/Main.kt
+++ src/main/kotlin/Laboratory_Work_No_1/Task1/Main.kt
@@ -1,16 +1,17 @@
 package Laboratory_Work_No_1.Task1
 
 fun main() {
-    val monitor1 = Display(1600, 900, 100.5f, "ASUS")
-    val monitor2 = Display(1920, 1200, 106.99f, "Samsung")
-    val monitor3 = Display(2560, 1080, 98.0f, "Dell")
-    monitor1.compareSize(monitor2)
-    monitor1.compareSharpness(monitor2)
-    monitor1.compareWithMonitor(monitor2)
-    monitor2.compareSize(monitor3)
-    monitor2.compareSharpness(monitor3)
-    monitor2.compareWithMonitor(monitor3)
-    monitor3.compareSize(monitor1)
-    monitor3.compareSharpness(monitor1)
-    monitor3.compareWithMonitor(monitor1)
+    val monitors = listOf(
+        Display(1600, 900, 100.5f, "ASUS"),
+        Display(1920, 1200, 106.99f, "Samsung"),
+        Display(2560, 1080, 98.0f, "Dell")
+    )
+    for (firstMonitor in monitors){
+        for (secondMonitor in monitors)
+            if (firstMonitor != secondMonitor){
+                firstMonitor.compareSize(secondMonitor)
+                firstMonitor.compareSharpness(secondMonitor)
+                firstMonitor.compareWithMonitor(secondMonitor)
+            }
+    }
 }
