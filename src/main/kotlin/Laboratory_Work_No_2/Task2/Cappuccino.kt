73954abd60a344f11f0bbb355a4a40735237329e
Mihaela Catan <mihaelacatan7@gmail.com>
2024-11-03 19:43:57 +0200

    Add printCoffeeDetails() method to each class, finish task 2

---
+++ src/main/kotlin/Laboratory_Work_No_2/Task2/Cappuccino.kt
@@ -0,0 +1,16 @@
+package Laboratory_Work_No_2.Task2
+
+import Laboratory_Work_No_2.Task1.Intensity
+
+// Base class for Cappuccino that inherits from Coffee
+open class Cappuccino(
+    private var intensity: Intensity,
+    private var mlOfMilk: Int,
+    private val coffee: String = "Cappuccino"
+) : Coffee(intensity, coffee) {
+    // Inheriting, overriding printCoffeeDetails from Coffee and making it a base class
+    public override fun printCoffeeDetails() {
+        super.printCoffeeDetails()
+        println("Adding $mlOfMilk ml of milk")
+    }
+}
